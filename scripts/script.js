1d9b3b559e50b21950a8d82a54054074598a4804
troylochner <troy.lochner@gmail.com>
2020-09-13 16:28:39 -0400

    Restructurinog overal setup to increace simplicity. Moing all to script.js file instead of breaking into separate JS files

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,28 @@
-//init the page
-//load anything stored in the local storage.
-//when document is ready - the user can do things. 
+$(document).ready(function () {
 
-$(document).ready(function(){
+    //PLACING API KEY AND URL AT THE TOP
+    var appKey = "&appid=a3aba25faf7e59594281cfb0824ca1d7";
+    var apiURL = "https://api.openweathermap.org/data/2.5/weather?";
+
+    //These may be a problem if left in this file?
+    var myWeather;
+    var myForecast;
+
+    //DECLARE DOCUMENT ELEMENT VARIABLES RIGHT HERE
+
+    //DEFINE SOME FUNCTIONS:
+
+    function loadPrevSearches();
+
+
     //SAVE DATA
     $("#btnSearchCity").on("click", function () {
         event.preventDefault()
         var cityName = $("#cityInput").val();
-        
+
         console.log(cityName)
-        
-        getWeatherByCity(cityName); 
+
+        getWeatherByCity(cityName);
         //console.log(myResult);
         var row = $("<div>").css("width", "100%").text('Weather for ' + cityName.toUpperCase() + myWeather);
         //row.JSONParse(myResult);
@@ -18,9 +30,93 @@ $(document).ready(function(){
 
 
     })
-   
+
+});
+
+function displayForecast();
+
+
+//WEATHER API FUNCTIONS
+//ICON REF : https://openweathermap.org/weather-conditions#How-to-get-icon-URL
+
+function getWeatherByCity(cityInput) {
+
+    var settings = {
+        "url": apiURL + "q=" + cityInput + appKey,
+        "method": "GET",
+        "timeout": 0,
+        success: function (data) {
+            myWeather = data;
+            console.log("getWeatherByCity -> myWeather", myWeather)
+
+        },
+        error: function (ex) {
+            alert(ex.data);
+        }
+    };
+    $.ajax(settings).done(function (response) {
+
     });
-    
+
+}
+
+//GET BY ZIP
+function getWeatherByZip(zipcode) {
+    var settings = {
+        "url": apiURL + "zip=" + zipcode + appKey,
+        "method": "GET",
+        "timeout": 0,
+        //Placed this into to code to get the variabout out of async response. 
+        success: function (data) {
+            myWeather = data;
+            console.log("getWeatherByZip -> myWeather", myWeather)
+
+        },
+        error: function (ex) {
+            alert(ex.data);
+        }
+    };
+    $.ajax(settings).done(function (response) {});
+
+}
+
+//GET FORCASE W/ ONECALL API
+function getWeatherForecast(lon, lat) {
+    var lon = "lon=" + lon;
+    var lat = "&lat=" + lat;
+    var exclude = "&exclude=current,minutely,hourly";
+    apiURL = "https://api.openweathermap.org/data/2.5/onecall?";
+
+    var settings = {
+
+        "url": apiURL + lon + lat + exclude + appKey,
+        "method": "GET",
+        "timeout": 0,
+        success: function (data) {
+            myForecast = data;
+            console.log("getWeatherForecast -> myForecast", myForecast)
+        },
+        error: function (ex) {
+            alert(ex.data);
+        }
+    };
+    $.ajax(settings).done(function (response) {})
+};
+
+//GET LAT LONG FROM MY WEATHER
+function getForecast(myWeather) {
+    var lat = myWeather.coord.lat;
+    var lon = myWeather.coord.lon;;
+    var myForecast = getWeatherForecast(lon, lat);;
+    console.log("getForecast -> myForecast", myForecast);
+    return myForecast;
+}
+
+
+
+
+//USEFUL SNIPPETS
+//Parse result
 
 
 /*
@@ -108,4 +204,3 @@ $(document).ready(function(){
     });
     
 */
-
